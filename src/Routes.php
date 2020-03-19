9294b84c9e53649d873904c58c7a1512cbaf7020
3PSY0N <3511559-3PSY0N@users.noreply.gitlab.com>
2020-03-19 23:11:24 +0100

    Add delete & purge routes

--- src/Routes.php
+++ src/Routes.php
@@ -15,18 +15,19 @@ Route\get('/', controller('App\Controllers\Home', 'displayHome'));
 Route\post('/', controller('App\Controllers\Home', 'upload'));
 
 Route\get('/si/([a-zA-Z0-9-+_~$]+)', controller('App\Controllers\Image', 'show'));
+Route\get('/di([\?]page=[0-9]+)?/?([\&]img=[a-zA-Z0-9-+_~$]+)', controller('App\Controllers\Image', 'delete'));
+
 Route\post('/pi/', controller('App\Api\ImageApi', 'postImage'));
 
 Route\get('/login', controller('App\Controllers\Login', 'getLogin'));
 Route\post('/login', controller('App\Controllers\Login', 'postLogin'));
-
 Route\get('/logout', controller('App\Controllers\UserProfile', 'logout'));
 
 Route\get('/profile?([\?]page=[0-9]+)?', controller('App\Controllers\UserProfile', 'displayUserProfile'));
 
-Route\get('/showprofile/([a-zA-Z0-9]+)', controller('App\Controllers\ShowProfile', 'showUserProfile'));
-
 Route\get('/gitstatus', controller('App\Controllers\GitStatus', 'showCommits'));
+
+Route\get('/admin/purge', controller('App\Handlers\ImgHandler', 'purge'));
 
 Route\get('.*', function () {
     (new Errors())->e404();
