57cf912da00452bb8bc3cb63f047e660f03c4826
3PSY0N <3511559-3PSY0N@users.noreply.gitlab.com>
2020-03-20 23:37:10 +0100

    Reorganize assets

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Core\Application;
 
 defined('ROOT') || define('ROOT', realpath(dirname(__DIR__)));
 
-require_once(ROOT . '/src/Core/config.php');
-require_once(ROOT . '/vendor/autoload.php');
+require_once ROOT . '/src/Core/config.php';
+require_once ROOT . '/vendor/autoload.php';
 
 (new Application())->startApp();
