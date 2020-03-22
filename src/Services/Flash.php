87c3da4a0b85445972f70be7aac782b24dac980c
3PSY0N <3511559-3PSY0N@users.noreply.gitlab.com>
2020-03-22 17:42:09 +0100

    Add title property nullable for flash messages

--- src/Services/Flash.php
+++ src/Services/Flash.php
@@ -19,8 +19,7 @@ class Flash
             foreach (Session::get('flashMsg') as $flash) {
                 $autoClose = ($flash['autoClose']) ? 'autoclose' : null;
 
-                $flashMsg .= '<div class="alert ' . $autoClose . ' alert-' . $flash['color'] . ' fade show" role="alert">';
-                $flashMsg .= $flash['message'];
+                $flashMsg .= '<div class="callout alert ' . $autoClose . ' ' . $flash['color'] . ' fade show" role="alert">';
 
                 if (is_null($autoClose)) {
                     $flashMsg .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
@@ -28,6 +27,11 @@ class Flash
                     $flashMsg .= '</button>';
                 }
 
+                if ($flash['title']) {
+                    $flashMsg .= '<span class="title">' . $flash['title'] . '</span>';
+                }
+
+                $flashMsg .= '<span>' . $flash['message'] . '</span>';
                 $flashMsg .= '</div>';
             }
 
@@ -40,19 +44,21 @@ class Flash
     /**
      * @param $color
      * @param $message
-     * @param bool $dismissBtn
+     * @param null $title
+     * @param bool $dismiss
      * @param null $redirectUrl
      */
-    public function setFlash($color, $message, $dismissBtn = false, $redirectUrl = null)
+    public function setFlash($color, $message, $title = null, $dismiss = false, $redirectUrl = null)
     {
         static $no_calls = 0;
         ++$no_calls;
         $_SESSION['flashMsg'][$no_calls]['color']     = $color;
+        $_SESSION['flashMsg'][$no_calls]['title']     = $title;
         $_SESSION['flashMsg'][$no_calls]['message']   = $message;
-        $_SESSION['flashMsg'][$no_calls]['dismiss']   = ($dismissBtn ? 'alert-dismissible' : null);
+        $_SESSION['flashMsg'][$no_calls]['dismiss']   = ($dismiss ? 'alert-dismissible' : null);
         $_SESSION['flashMsg'][$no_calls]['autoClose'] = false;
 
-        if (!$dismissBtn) {
+        if (!$dismiss) {
             $_SESSION['flashMsg'][$no_calls]['autoClose'] = true;
         }
 
