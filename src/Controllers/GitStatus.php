8cb200a77ffcfd3df32c678a4fe239a52f7851d1
3PSY0N <3511559-3PSY0N@users.noreply.gitlab.com>
2020-03-24 17:03:08 +0100

    Add exception if no gitlab config is set Fix bad css class

--- src/Controllers/GitStatus.php
+++ src/Controllers/GitStatus.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Core\Twig;
+use App\Services\Flash;
 use App\Services\Session;
 use Gitlab\Client;
 
@@ -10,23 +11,30 @@ class GitStatus
 {
     private $token = GITLAB_API_TOKEN;
     private $projectId = GITLAB_PROJECT_ID;
+    private $flash;
+    private $twig;
 
     public function __construct()
     {
         $this->twig = new Twig();
+        $this->flash = new Flash();
     }
 
     public function showCommits()
     {
-        $client = Client::create('https://gitlab.com')
-                ->authenticate($this->token, Client::AUTH_URL_TOKEN);
+        try {
+            $client = Client::create('https://gitlab.com');
+            $client->authenticate($this->token, Client::AUTH_URL_TOKEN);
 
-        $repository = $client->repositories();
+            $repository = $client->repositories();
 
-        $getLastMergeRequest = $client->mergeRequests()->all($this->projectId)[0];
-        $getLastCommits      = $repository->commits($this->projectId);
-        $getIssues           = $client->issues()->all();
-        $getLastRelease      = $repository->releases($this->projectId);
+            $getLastMergeRequest = $client->mergeRequests()->all($this->projectId)[0];
+            $getLastCommits      = $repository->commits($this->projectId);
+            $getIssues           = $client->issues()->all();
+            $getLastRelease      = $repository->releases($this->projectId);
+        } catch (\Exception $e) {
+            $this->flash->setFlash('info', 'Dev Status is unavailable at this time.', 'Dev Status', false, '/');
+        }
 
         echo $this->twig->render('gitstatus.twig', [
             'profileLink'      => Session::get('userName'),
